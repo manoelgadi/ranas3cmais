2e9b32ef3f3a972dccc4108916e4de6efa6b12c6
Prof. Manoel Gadi <manoelgadi@gmail.com>
2021-09-28 12:25:11 +0200

    Update ranas_js.js

--- static/ranas_js.js
+++ static/ranas_js.js
@@ -1,5 +1,5 @@
 function mueverana(elemento){
-	//alert('cambiado 11:52');
+	//alert('cambiado 12:20');
 	var id_num = parseInt(elemento.id,10), destino = -1;
 	if(elemento.src.includes("R.png")){
 		if(// id_num-1 >= 0 -- arregla la causa
@@ -7,9 +7,9 @@ function mueverana(elemento){
 		document.getElementById(id_num-1).src.includes("B.png")){
 			destino = id_num-1;
 		} else if(//id_num-2 >= 0 -- arregla la causa 
-		document.getElementById(id_num-2) != null && // arregla el sintoma
-		document.getElementById(id_num-2).src.includes("B.png")){
-			destino = id_num-2;
+			document.getElementById(id_num-2) != null && // arregla el sintoma
+			document.getElementById(id_num-2).src.includes("B.png")){
+				destino = id_num-2;
 		}
 	} else if(elemento.src.includes("L.png")){
 		try {  // arregla el muerto - hace una cirurgia
@@ -23,11 +23,27 @@ function mueverana(elemento){
 		}
 		
 	}	
-	
 	if (destino>=0){
-		var aux =  elemento.src;
-		elemento.src = document.getElementById(destino).src;
-		document.getElementById(destino).src = aux;
+		cambia_ranas(elemento.id,destino);
 	}
 }
 
+function cambia_ranas(i,j){
+	var aux =  document.getElementById(i).src;
+	document.getElementById(i).src = document.getElementById(j).src;
+	document.getElementById(j).src = aux;
+	//
+}
+
+function aleatoriza_ranas(){
+	for(var origen=0;origen<=6;origen++){
+		do {
+			destino = Math.floor(Math.random() * 7);
+		} while(origen == destino);
+		cambia_ranas(origen,destino);
+	}
+}
+
+function deshace_ultimo(){
+	
+}
